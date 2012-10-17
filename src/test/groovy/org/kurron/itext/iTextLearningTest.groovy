7177c52b4f39b227a39966ea479e4d0a928afe13
Ronald D. Kurr <kurr@kurron.org>
2012-10-17 16:55:48 -0400

    Porting the Unicode example from Clown PDF is, unfortunately, not working.

--- src/test/groovy/org/kurron/itext/iTextLearningTest.groovy
+++ src/test/groovy/org/kurron/itext/iTextLearningTest.groovy
@@ -7,6 +7,7 @@ import com.itextpdf.text.Paragraph
 import com.itextpdf.text.Chunk
 import com.itextpdf.text.BaseColor
 import com.itextpdf.text.Font
+import com.itextpdf.text.pdf.BaseFont
 
 /**
  * Trying to figure out how the iText library works.
@@ -22,8 +23,19 @@ class iTextLearningTest extends Specification
 
         when: "page is built"
         document.open()
-        Font font = new Font( Font.FontFamily.HELVETICA, 14, Font.BOLD, BaseColor.BLACK );
-        Chunk chunk = new Chunk( "Hello, World!", font );
+        BaseFont base = BaseFont.createFont( "/home/ron/git/itext-experiment/src/test/resources/GenR102.TTF", BaseFont.CP1250, BaseFont.EMBEDDED )
+        println 'encoding = ' + base.encoding
+        base.fullFontName.each {
+            println 'fullFontName = ' + it
+        }
+        base.familyFontName.each {
+            println 'familyFontName = ' + it
+        }
+        base.codePagesSupported.each {
+            println 'code page = ' + it
+        }
+        Font font = new Font( base, 24 )
+        Chunk chunk = new Chunk( "Aduniya kuna n gu ibuna damayo hɛi nɔ dei-dei nn daama nna n burucinitɛrɛ fɔ, n lasabu nna laakari ya nam nn mɔ huro cɛrɛ kuna nyanze tɛrɛ bɔŋɔɔ.", font );
         document.add( chunk )
         document.add( Chunk.NEWLINE )
         document.close()
