38a4ae98809a768890f34150974bcaf300f311b0
Ronald D. Kurr <kurr@kurron.org>
2012-10-17 11:50:43 -0400

    Hello World is working.

--- src/test/groovy/org/kurron/itext/iTextLearningTest.groovy
+++ src/test/groovy/org/kurron/itext/iTextLearningTest.groovy
@@ -1,6 +1,9 @@
 package org.kurron.itext
 
 import spock.lang.Specification
+import com.itextpdf.text.Document
+import com.itextpdf.text.pdf.PdfWriter
+import com.itextpdf.text.Paragraph
 
 /**
  * Trying to figure out how the iText library works.
@@ -10,7 +13,14 @@ class iTextLearningTest extends Specification
     def "hello"()
     {
         given: "document"
+        Document document = new Document()
+        PdfWriter writer = PdfWriter.getInstance( document, new FileOutputStream( "hello.pdf" ) )
+
         when: "page is built"
+        document.open()
+        document.add( new Paragraph( "Hello, World!" ) )
+        document.close()
+
         then: "pdf file is created"
     }
 }
